fc4ff061b75dde3b46338d3d60e0d5197d7f86de
Nam Nguyen <nlnam@fossil.com>
2018-03-19 18:45:27 +0700

    Clean up solution 1

--- src/problem1/1.js
+++ src/problem1/1.js
@@ -7,6 +7,6 @@
  */
 export default function ultraTrim(input) {
   let result = input.trim()
-  result = result.replace(/([\t\s])+/g, '$1')
+  result = result.replace(/([\s])+/g, '$1')
   return result
 }
